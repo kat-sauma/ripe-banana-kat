bc101cf3094e7fa8c79a7e15690cd25d8aee8be6
Kat Sauma <katsauma@gmail.com>
2021-04-09 09:43:26 -0700

    initial push

---
+++ __tests__/app.test.js
@@ -0,0 +1,9 @@
+
+const request = require('supertest');
+const app = require('../lib/app');
+const sequelize = require('../lib/utils/database')
+
+describe('ripe-banana-kat routes', () => {
+  beforeEach(() => {
+  });
+});
