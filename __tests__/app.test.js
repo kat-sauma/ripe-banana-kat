c9a594503fe080b3c46f06099b13990ead813917
Kat Sauma <katsauma@gmail.com>
2021-04-09 12:22:59 -0700

    written test goes red

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,9 +1,27 @@
 
 const request = require('supertest');
 const app = require('../lib/app');
-const sequelize = require('../lib/utils/database')
+const db = require('../lib/utils/database')
 
 describe('ripe-banana-kat routes', () => {
   beforeEach(() => {
+    return db.sync({force: true});
+  });
+
+  it('creates an actor and adds them to the database', async () => {
+    const actor = await request(app)
+    .post('/api/v1/actors')
+    .send({
+      name: 'Reese Witherspoon',
+      dob: 'March 26, 1976',
+      pob: 'New Orleans, Louisiana'
+    });
+
+    expect(actor.body).toEqual({
+      id: expect.any(String),
+      name: 'Reese Witherspoon',
+      dob: 'March 26, 1976',
+      pob: 'New Orleans, Louisiana'
+    });
   });
 });
